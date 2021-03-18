8acc96cfa6fcf53fdca44cbbc011c9fc76b9c21d
justheuristic <justheuristic@gmail.com>
2021-03-18 22:31:42 +0300

    expiration_time -> 5

--- collaboration.py
+++ collaboration.py
@@ -21,7 +21,7 @@ logger = logging.getLogger(__name__)
 class CollaborationArguments:
     initial_peers: str  # one or more peers (comma-separated) that will welcome you into the collaboration
     dht_key_for_averaging: str = 'colab_wikitext'  # a unique identifier of this experimental run's metadata
-    averaging_expiration: float = 3.0  # averaging group will expire after this many seconds
+    averaging_expiration: float = 5.0  # averaging group will expire after this many seconds
     averaging_step_timeout: float = 30.0  # give up averaging step after this many seconds
     metadata_expiration: float = 15  # peer's metadata will be removed if not updated in this many seconds
     target_group_size: int = 64      # maximum group size for all-reduce
