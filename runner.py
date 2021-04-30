30f56979410f2da9f29451a510544891e4939939
Michael Diskin <yhn1124@gmail.com>
2021-05-01 00:17:08 +0300

    Add syslog logging for colab

---
+++ runner.py
@@ -0,0 +1,25 @@
+import subprocess
+import socket
+import shlex
+
+
+def syslog(message, host, level=6, facility=1,  port=514):
+    """
+    Send syslog UDP packet to given host and port.
+    """
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    data = '<%d> %s' % (level + facility*8, message)
+    sock.sendto(bytes(data, "ascii"), (host, port))
+    sock.close()
+
+
+def run_with_logging(command, address):
+    proc = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+
+    while True:
+        output = proc.stdout.readline().rstrip()
+        if proc.poll() is not None:
+            break
+        if output:
+            print(output)
+            syslog(output, host=address)
